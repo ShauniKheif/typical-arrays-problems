6d5a07702f044f16f06a7b54cca8d3c664e6b63f
ShauniKheif <53867436+ShauniKheif@users.noreply.github.com>
2021-03-03 13:51:07 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!Array.isArray(array)) {
+    return 0;
+  }
+  else if (array.length == 0) {
+    return 0;
+  }
+  else {
+    return Math.min.apply(Math, array);
+  }
 }
-
 exports.max = function max (array) {
-  return 0;
+  if (!Array.isArray(array)) {
+    return 0;
+  }
+  else if (array.length == 0) {
+    return 0;
+  }
+  else {
+    return Math.max.apply(Math, array);
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!Array.isArray(array)) {
+    return 0;
+  }
+  else if (array.length == 0) {
+    return 0;
+  }
+  else {
+    let result = 0;
+    for (let i=0; i<array.length; i++) {
+      result += array[i];
+    }
+    result /= array.length;
+    return (result);
+  }
 }
